c445c4d32d3b75089c68c27b3660ae21bcf49009
Rajya Vardhan <vardhanrajya08@gmail.com>
2023-04-21 01:33:03 +0530

    RTT

---
+++ Round_Trip_Time.py
@@ -0,0 +1,28 @@
+# Python program to calculate RTT
+
+# code starts
+import time
+import requests
+
+# Function to calculate the RTT
+def RTT(url):
+
+	# time when the signal is sent
+	t1 = time.time()
+
+	r = requests.get(url)
+
+	# time when acknowledgement of signal
+	# is received
+	t2 = time.time()
+
+	# total time taken
+	tim = str(t2-t1)
+
+	print("Time in seconds :" + tim)
+
+# driver program
+# url address
+url = "http://www.google.com"
+RTT(url)
+# code ends
